85b87023468328c81c8240ba71aba936b04e65d9
Tristan Marsolais <tristan.marsolais@hotmail.com>
2022-03-26 03:28:31 -0400

    adding state functionality

--- src/state.rs
+++ src/state.rs
@@ -4,4 +4,15 @@ use serde::{Deserialize, Serialize};
 use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Account {
+    pub accountHolder: Addr,
+    pub paramAccTimeBeforeLost: Date,
+    pub accDateOfLost: Date,
+    pub executers: Tuple, 
+    pub paramExeTimeBeforeLost: Date,
+    pub exeDateOfLost: Date,
+    pub vault: tokens,
+}
 
+pub const ACCOUNT: Item<Account> = Item::new("account");
