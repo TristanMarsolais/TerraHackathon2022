318785fe9f289531138d5a57e3a3c4e92705da44
Tristan Marsolais <tristan.marsolais@hotmail.com>
2022-03-26 03:41:18 -0400

    adding msg functions

--- src/msg.rs
+++ src/msg.rs
@@ -8,6 +8,10 @@ pub struct InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
+    CreateAccount {},
+    ModifyAccount {}, 
+    AHStillAlive {}, 
+    
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
